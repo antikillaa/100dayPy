6cddcd6c008712daa573f56403a0a7a344c37359
Stanislav Peshkur <stanislav.peshkur@leaseplan.com>
2022-05-23 22:18:28 -0700

    Added score handled snake game

---
+++ src/oop/day_24/main.py
@@ -0,0 +1,5 @@
+# with open("my_file.txt") as file:
+#     print(file.read())
+
+with open("my_file.txt", mode='w') as file:
+    file.write("Hello world")
--- src/oop/snake_game_with_high_score/score.py
+++ src/oop/snake_game_with_high_score/score.py
@@ -6,14 +6,20 @@ FONT = ('Courier', 24, 'bold')
 class Score(Turtle):
     def __init__(self):
         super().__init__()
+        self.high_score = None
         self.score = 0
-        self.high_score = 0
         self.penup()
         self.goto(0, 270)
         self.color("white")
         self.hideturtle()
         self.speed(0)
+        self.get_high_score()
         self.update_score()
+
+    def get_high_score(self):
+        with open("score.txt") as file:
+            score = file.read()
+            self.high_score = int(score)
 
     def update_score(self):
         self.write(f"Your score is: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
@@ -31,6 +37,8 @@ class Score(Turtle):
         self.clear()
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("score.txt", mode="w") as file:
+                file.write(str(self.score))
         self.score = 0
         self.update_score()
 
