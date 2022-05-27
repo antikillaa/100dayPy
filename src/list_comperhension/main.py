f68d90ccdc91f8b3fa515f12509799823b9c86eb
Stanislav Peshkur <stanislav.peshkur@leaseplan.com>
2022-05-26 17:35:56 -0700

    List Comprehension

---
+++ src/list_comperhension/main.py
@@ -0,0 +1,9 @@
+# new_list = [i+1 for i in numbers]
+name = "Stan"
+new_list = [l for l in name]
+new_list = [l for l in name[1:]]
+new_list = [l for l in name[2:]]
+numbers_list = [n*n for n in range(1, 5)]
+numbers_list = [n+n for n in range(1, 5)]
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+names[1].upper()
