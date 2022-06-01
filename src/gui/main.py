ebc9f949c961c9ddb710ccf0299225b5005c9fc7
Stanislav Peshkur <speshkur@aligntech.com>
2022-06-01 15:33:11 -0700

    Added tkinter base code

--- src/gui/main.py
+++ src/gui/main.py
@@ -6,5 +6,22 @@ window.minsize(width=500, height=300)
 
 # Label
 my_label = tkinter.Label(text="I am a lebel", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text=input.get())
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+#Entry
+input = tkinter.Entry(width=10)
+input.pack()
+
+canvas = tkinter.Text()
+canvas.pack()
 
 window.mainloop()
--- src/gui/playground.py
+++ src/gui/playground.py
@@ -5,4 +5,27 @@ def add(*args):
     return summa
 
 
-print(add(1, 2, 5, 6, 4, 3, 2, 4, 6, 7, 0))
+# print(add(1, 2, 5, 6, 4, 3, 2, 4, 6, 7, 0))
+
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get('make')
+        self.model = kw.get('model')
+        self.colour = kw.get('colour')
+        self.seats = kw.get('seats')
+
+
+my_car = Car(make='Nissan', model='GT-R')
+
+print(my_car.model)
