b31c1f5fb7a826e9e0e2d916116cad6a057bad80
Stanislav Peshkur <stanislav.peshkur@leaseplan.com>
2022-05-31 22:23:47 -0700

    added nato dict

--- src/dict_comperhension/NATO-alphabet/main.py
+++ src/dict_comperhension/NATO-alphabet/main.py
@@ -1,31 +1,32 @@
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+# Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
+    # Access key and value
     pass
 
 import pandas
+
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
+# Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#1. Create a dictionary in this format:
+# 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-#2. Create a list of the phonetic code words from a word that the user inputs.
+# 2. Create a list of the phonetic code words from a word that the user inputs.
 
 game_is_on = True
 while game_is_on:
@@ -37,5 +38,3 @@ while game_is_on:
     nato_words = [nato_dict[v] for v in word]
 
     print(nato_words)
-
-
