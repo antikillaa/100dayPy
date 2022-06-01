370c887b5684813aa4257dd0ccfe587e1fc3315d
Stanislav Peshkur <stanislav.peshkur@leaseplan.com>
2022-05-31 22:23:47 -0700

    added nato dict

--- src/dict_comperhension/NATO-alphabet/main.py
+++ src/dict_comperhension/NATO-alphabet/main.py
@@ -20,12 +20,12 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+#1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+#2. Create a list of the phonetic code words from a word that the user inputs.
 
 game_is_on = True
 while game_is_on:
