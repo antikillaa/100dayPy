dc3f51184428d71c4b023f707300376bc1907b92
Stanislav Peshkur <stanislav.peshkur@leaseplan.com>
2022-06-22 19:37:03 -0700

    web scrapping live site

---
+++ src/html/beautiful-soup/live_site.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://news.ycombinator.com/")
+
+soup = BeautifulSoup(response.text, "html.parser")
+# articles = {text.getText():  text.get("href") for text in soup.find_all(name="a", class_="titlelink")}
+# article_upvote = int(soup.find(name="span", class_="score").getText().split(" ")[0])
+
+article_tag = soup.find_all(name="a", class_="titlelink")
+article_texts = [text.getText() for text in soup.find_all(name="a", class_="titlelink")]
+article_links = [text.get("href") for text in soup.find_all(name="a", class_="titlelink")]
+article_up_votes = [int(text.getText().split()[0]) for text in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_up_votes)
+max_up_votes = article_up_votes.index(largest_number)
+
+print(article_texts[max_up_votes], article_links[max_up_votes], article_up_votes[max_up_votes])
+
+print(article_texts)
+print(article_links)
+print(article_up_votes)
