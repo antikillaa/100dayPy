3afaaf6975f2ac7a6da5f952ac02660d3146ea88
Stanislav Peshkur <speshkur@aligntech.com>
2022-06-23 09:30:12 -0700

    Added csv and xls export to the live_site.py

--- src/html/beautiful-soup/live_site.py
+++ src/html/beautiful-soup/live_site.py
@@ -1,3 +1,4 @@
+import pandas as pandas
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,7 +11,7 @@ soup = BeautifulSoup(response.text, "html.parser")
 article_tag = soup.find_all(name="a", class_="titlelink")
 article_texts = [text.getText() for text in soup.find_all(name="a", class_="titlelink")]
 article_links = [text.get("href") for text in soup.find_all(name="a", class_="titlelink")]
-article_up_votes = [int(text.getText().split()[0]) for text in soup.find_all(name="span", class_="score")]
+article_up_votes = [text.getText().split()[0] if text.getText().split()[0] else 0 for text in soup.find_all(name="span", class_="score")]
 
 largest_number = max(article_up_votes)
 max_up_votes = article_up_votes.index(largest_number)
@@ -20,3 +21,13 @@ print(article_texts[max_up_votes], article_links[max_up_votes], article_up_votes
 print(article_texts)
 print(article_links)
 print(article_up_votes)
+
+articles = {
+    "title": article_texts,
+    "link": article_links,
+    # "rating": article_up_votes
+}
+
+data = pandas.DataFrame(articles)
+data.to_html("articles.html", encoding="UTF-8")
+data.to_excel("articles.xls", encoding="UTF-8")
